4de302d1a63e6b5a6a07d0646dca2ae6201bf1b5
Joshua Realiquez <jrealiquez-2019342@kinal.edu.gt>
2026-09-15 23:54:50 -0600

    feat: se realiza el diseño de Swagger/OpenAPI para la entidad Libro

---
+++ src/main/java/org/realiquez/lab_VI/LabViApplication.java
@@ -0,0 +1,13 @@
+package org.realiquez.lab_VI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabViApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabViApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/realiquez/lab_VI/LabViApplicationTests.java
@@ -0,0 +1,13 @@
+package org.realiquez.lab_VI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabViApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
